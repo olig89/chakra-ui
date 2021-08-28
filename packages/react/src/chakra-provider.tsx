a1b9bcb8e80056124d8f7bdc41a2d81059566a00
Oli Gill <oliver.gill.89@gmail.com>
2021-08-28 16:09:15 +0300

    web only

--- packages/react/src/chakra-provider.tsx
+++
@@ -1,8 +0,0 @@
-import { ChakraProvider as BaseChakraProvider } from "@chakra-ui/provider"
-import defaultTheme from "@chakra-ui/theme"
-
-export const ChakraProvider = BaseChakraProvider
-
-ChakraProvider.defaultProps = {
-  theme: defaultTheme,
-}
